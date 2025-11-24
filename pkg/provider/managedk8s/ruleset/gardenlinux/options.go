180a9a289bf3930e36e5b57baf246f13b481a133
georgibaltiev <georgi.baltiev@sap.com>
2025-11-24 08:44:41 +0200

    add: rule options

---
+++ pkg/provider/managedk8s/ruleset/gardenlinux/options.go
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gardenlinux
+
+import (
+	"log/slog"
+
+	"k8s.io/client-go/rest"
+)
+
+// CreateOption is a function that acts on a [Ruleset]
+// and is used to construct such objects.
+type CreateOption func(*Ruleset)
+
+// WithVersion sets the version of a [Ruleset].
+func WithVersion(version string) CreateOption {
+	return func(r *Ruleset) {
+		r.version = version
+	}
+}
+
+// WithConfig sets the Config of a [Ruleset].
+func WithConfig(config *rest.Config) CreateOption {
+	return func(r *Ruleset) {
+		r.Config = config
+	}
+}
+
+// WithLogger the logger of a [Ruleset].
+func WithLogger(logger *slog.Logger) CreateOption {
+	return func(r *Ruleset) {
+		r.logger = logger
+	}
+}
+
+func WithArgs(args Args) CreateOption {
+	return func(r *Ruleset) {
+		if len(args.NodeGroupByLabels) > 0 {
+			r.args.NodeGroupByLabels = args.NodeGroupByLabels
+		}
+	}
+}
