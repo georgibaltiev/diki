244921c5a28bca63232f188ec3f44b70b8145967
georgibaltiev <georgi.baltiev@sap.com>
2025-11-24 08:44:48 +0200

    add: rule implementaiton

---
+++ pkg/provider/managedk8s/ruleset/gardenlinux/ruleset.go
@@ -0,0 +1,209 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gardenlinux
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"sync"
+	"time"
+
+	"github.com/gardener/diki/imagevector"
+	"github.com/gardener/diki/pkg/config"
+	"github.com/gardener/diki/pkg/kubernetes/pod"
+	"github.com/gardener/diki/pkg/rule"
+	"github.com/gardener/diki/pkg/ruleset"
+	"github.com/gardener/diki/pkg/shared/images"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
+	sharedrules "github.com/gardener/diki/pkg/shared/ruleset/disak8sstig/rules"
+)
+
+const (
+	// RulesetID is a constant containing the id of the Security Hardened Kubernetes Cluster Ruleset.
+	RulesetID = "gardenlinux"
+	// RulesetName is a constant containing the user-friendly name of the Security Hardened Kubernetes ruleset.
+	RulesetName = "Gardenlinux Ruleset"
+)
+
+var (
+	_                 ruleset.Ruleset = &Ruleset{}
+	SupportedVersions                 = []string{"alpha"}
+)
+
+type Ruleset struct {
+	version           string
+	Config            *rest.Config
+	Client            client.Client
+	ClusterPodContext pod.SimplePodContext
+	logger            *slog.Logger
+	args              Args
+}
+
+type Args struct {
+	NodeGroupByLabels []string `json:"nodeGroupByLabels" yaml:"nodeGroupByLabels"`
+}
+
+func (r *Ruleset) ID() string {
+	return RulesetID
+}
+
+func (r *Ruleset) Name() string {
+	return RulesetName
+}
+
+func (r *Ruleset) Version() string {
+	return r.version
+}
+
+// New creates a new Ruleset.
+func New(options ...CreateOption) (*Ruleset, error) {
+	r := &Ruleset{}
+
+	for _, o := range options {
+		o(r)
+	}
+
+	c, err := client.New(r.Config, client.Options{})
+	if err != nil {
+		return nil, err
+	}
+	r.Client = c
+
+	podContext, err := pod.NewSimplePodContext(r.Client, r.Config, nil)
+	if err != nil {
+		return nil, err
+	}
+	r.ClusterPodContext = *podContext
+
+	return r, nil
+}
+
+// FromGenericConfig creates a Ruleset from a RulesetConfig
+func FromGenericConfig(rulesetConfig config.RulesetConfig, managedConfig *rest.Config, fldPath *field.Path) (*Ruleset, error) {
+	rulesetArgsByte, err := json.Marshal(rulesetConfig.Args)
+	if err != nil {
+		return nil, err
+	}
+
+	var rulesetArgs Args
+	if err := json.Unmarshal(rulesetArgsByte, &rulesetArgs); err != nil {
+		return nil, err
+	}
+
+	ruleset, err := New(
+		WithVersion(rulesetConfig.Version),
+		WithConfig(managedConfig),
+		WithArgs(rulesetArgs),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return ruleset, nil
+}
+
+func (r *Ruleset) Run(ctx context.Context) (ruleset.RulesetResult, error) {
+	image, err := imagevector.ImageVector().FindImage(images.TestsNgImageName)
+	if err != nil {
+		return ruleset.RulesetResult{}, fmt.Errorf("failed to find image version for %s: %w", images.TestsNgImageName, err)
+	}
+
+	nodes, err := kubeutils.GetNodes(ctx, r.Client, 300)
+	if err != nil {
+		return ruleset.RulesetResult{}, err
+	}
+
+	allClusterPods, err := kubeutils.GetPods(ctx, r.Client, "", labels.NewSelector(), 300)
+	if err != nil {
+		return ruleset.RulesetResult{}, nil
+	}
+
+	nodesAllocatablePods := kubeutils.GetNodesAllocatablePodsNum(allClusterPods, nodes)
+
+	selectedNodes, checkResults := kubeutils.SelectNodes(nodes, nodesAllocatablePods, r.args.NodeGroupByLabels)
+
+	const systemNamespace = "kube-system"
+
+	wg := sync.WaitGroup{}
+	chResultChan := make(chan (rule.CheckResult))
+
+	for _, node := range selectedNodes {
+		wg.Add(1)
+		go func() {
+			var podName = fmt.Sprintf("test-ng-%s-%s", r.ID(), sharedrules.Generator.Generate(10))
+
+			defer func() {
+				wg.Done()
+				timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+				defer cancel()
+
+				if err := r.ClusterPodContext.Delete(timeoutCtx, podName, systemNamespace); err != nil {
+					r.logger.Error(err.Error())
+				}
+			}()
+
+			_, err := r.ClusterPodContext.Create(ctx, pod.NewGardenlinuxTestPod(podName, systemNamespace, image.String(), node.Name, nil))
+			if err != nil {
+				chResultChan <- rule.ErroredCheckResult(err.Error(), rule.NewTarget())
+				return
+			}
+
+			// here i dont do error checking, since it is expected behaviour for the test-ng pod to error when there are failings
+			r.ClusterPodContext.WaitPodCompleted(ctx, podName, systemNamespace)
+
+			logs, err := kubeutils.GetPodLogs(ctx, r.Config, podName, systemNamespace)
+			if err != nil {
+				chResultChan <- rule.ErroredCheckResult(err.Error(), rule.NewTarget())
+				return
+			}
+
+			// here we actually have to perform the parsing
+			fmt.Print(logs)
+
+			chResultChan <- rule.PassedCheckResult("Passed", rule.NewTarget())
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(chResultChan)
+	}()
+
+	for checkResult := range chResultChan {
+		checkResults = append(checkResults, checkResult)
+	}
+
+	return ruleset.RulesetResult{
+		RulesetID:      RulesetID,
+		RulesetVersion: r.version,
+		RulesetName:    RulesetName,
+		RuleResults: []rule.RuleResult{
+			{
+				RuleID:       "1000",
+				CheckResults: checkResults,
+			},
+		},
+	}, nil
+}
+
+// for now, we will not be able to run a specific rule, since the implementation is stored externally
+func (r *Ruleset) RunRule(_ context.Context, _ string) (rule.RuleResult, error) {
+	return rule.RuleResult{}, fmt.Errorf("ruleset gardenlinux does not support running rules individually")
+}
+
+// Logger returns the Ruleset's logger.
+// If not set it set it to slog.Default().With("ruleset", r.ID(), "version", r.Version() then return it.
+func (r *Ruleset) Logger() *slog.Logger {
+	if r.logger == nil {
+		r.logger = slog.Default().With("ruleset", r.ID(), "version", r.Version())
+	}
+	return r.logger
+}
